cadc457fac63f3095fee4e5b1f1789a9d63791dd
VictoriaLopes <66701008+VictoriaLopes@users.noreply.github.com>
2023-06-15 19:45:45 -0300

    consultarMesasDisponibles y verificarMesasDisponibles

--- ABM_RESTAURANT/src/Gestores/GestorMesa.java
+++ ABM_RESTAURANT/src/Gestores/GestorMesa.java
@@ -1,6 +1,7 @@
 package Gestores;
 import Clases.Mesa;
 
+import Clases.Situacion;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
@@ -54,7 +55,37 @@ public class GestorMesa {
 //        }catch(Exception e){
 //            e.printStackTrace();
 //        }
-//    }
+  //  }
+    public boolean verificarMesaDisponible(int numeroMesa) {
+        try {
+            List<Mesa> mesas = leerMesa();
+            for (Mesa mesa : mesas) {
+                if (mesa.getNumMesa() == numeroMesa && mesa.getSituacion() == Situacion.LIBRE) {
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error al leer los datos del archivo: " + e.getMessage());
+        }
+        return false;
+    }
+
+    public List<Mesa> consultarMesasDisponibles() {
+        List<Mesa> mesasDisponibles = null;
+        try {
+            List<Mesa> mesas = leerMesa();
+            mesasDisponibles = new ArrayList<>();
+
+            for (Mesa mesa : mesas) {
+                if (mesa.getSituacion() == Situacion.LIBRE) {
+                    mesasDisponibles.add(mesa);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error al leer los datos del archivo: " + e.getMessage());
+        }
+        return mesasDisponibles;
+    }
 
     @Override
     public String toString() {
